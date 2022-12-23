f8c08a3bfa6f2bbdaad01ff973bcaacc5f80ecda
kamala-github <agnes.kamaladevi@gmail.com>
2022-12-23 15:05:46 -0500

    Arrays

---
+++ array.js
@@ -0,0 +1,7 @@
+let names=[]
+console.log(names)
+names=['Mari','Edi','Arun','Arthi','Devaki'] //string array
+let ages =[10,20,30,40,50]
+console.log(names[0])
+console.log(ages.indexOf())
+console.log(names.indexOf('Arun'))
